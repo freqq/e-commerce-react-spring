c563070a8b8bcf31f43203538113839cfdbdf743
Przemyslaw Wit <dobrzeprzemyslane@gmail.com>
2019-04-16 15:29:17 +0200

    Fixed account-service dependencies problem

---
+++ server/zuul-server/src/main/java/com/freq/zuulserver/config/SwaggerDocumentation.java
@@ -0,0 +1,32 @@
+package com.freq.zuulserver.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Primary
+@EnableAutoConfiguration
+public class SwaggerDocumentation implements SwaggerResourcesProvider {
+    @Override
+    public List<SwaggerResource> get(){
+        List<SwaggerResource> resources = new ArrayList<>();
+
+        resources.add(swaggerResource("account-service", "/api/account/v1/api-docs", "1.0"));
+
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version){
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+}
