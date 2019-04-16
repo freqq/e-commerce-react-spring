987228cbd1a1971751173573766d04ccf2f62bb0
Przemyslaw Wit <dobrzeprzemyslane@gmail.com>
2019-04-16 14:31:46 +0200

    Initialized account-service

---
+++ server/account-service/src/main/java/com/freq/accountservice/AccountServiceApplication.java
@@ -0,0 +1,17 @@
+package com.freq.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@ComponentScan({"com.freq.config", "com.freq.accountservice"})
+@EnableSwagger2
+public class AccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+}
